7daaa8506dc10faf7cb7b2614fb38df245baaf20
Szymon Lewandowski <szymon.levy123@gmail.com>
2025-10-01 19:58:36 +0200

    cleanup domains naming in global config

--- public/index.php
+++ public/index.php
@@ -7,7 +7,7 @@ if(preg_match('/[A-Z]/', $_SERVER['REQUEST_URI'])) {
   header("Location: //" . $_SERVER['HTTP_HOST'] . strtolower($_SERVER['REQUEST_URI']));
 }
 
-// new router
+// router
 $router = new Core\Routing\Router($globals_container);
 $globals_container->set('router', $router);
 $routes = require_once $app_root . '/src/routes.php';
