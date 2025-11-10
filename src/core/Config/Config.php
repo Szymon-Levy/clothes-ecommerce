181a19299609b4812e3e444a018949ce7a192aef
Szymon Lewandowski <szymon.levy123@gmail.com>
2025-11-10 18:23:59 +0100

    clean Config class from redundant code

--- src/core/Config/Config.php
+++ src/core/Config/Config.php
@@ -6,27 +6,14 @@ use Exception;
 
 class Config
 {
+    protected string $configDir;
     protected array $config = [];
     protected array $excludes = ['main.config.php'];
 
     public function __construct()
     {
-        $dir = dirname(__DIR__, 3) . '/config';
-
-        $config_filles_dir = array_diff(scandir($dir), array('..', '.'));
-
-        foreach($config_filles_dir as $file_name) {
-            if (in_array($file_name, $this->excludes)) {
-                continue;
-            }
-            
-            $file_config = require $dir . '/' . $file_name;
-            
-            if (is_array($file_config)) {
-                [$name, $data] = $file_config;
-                $this->config[$name] = $data;
-            }
-        }
+        $this->configDir = dirname(__DIR__, 3) . '/config';
+        $this->getConfigFromFiles();
     }
 
     public function all()
@@ -36,80 +23,59 @@ class Config
 
     public function database(string $variable = '')
     {
-        $key = $_SERVER['SERVER_NAME'] == 'localhost' ? 'database_dev' : 'database_prod';
+        $option = $_SERVER['SERVER_NAME'] == 'localhost' ? 'database_dev' : 'database_prod';
 
-        if (! isset($this->config[$key])) {
-            throw new Exception("Database config doesn't exist");
-        }
-
-        $config = $this->config[$key];
-
-        if ($variable === '') {
-            return $config;
-        }
-
-        if (! array_key_exists($variable, $config)) {
-            throw new Exception("Variable {$variable} doesn't exist in database config");
-        }
-
-        return $config[$variable];
+        return $this->get($option, $variable);
     }
 
     public function email(string $variable = '')
     {
-
-        if (! isset($this->config['email'])) {
-            throw new Exception("Email config doesn't exist");
-        }
-
-        $config = $this->config['email'];
-
-        if ($variable === '') {
-            return $config;
-        }
-
-        if (! array_key_exists($variable, $config)) {
-            throw new Exception("Variable {$variable} doesn't exist in email config");
-        }
-
-        return $config[$variable];
+        return $this->get('email', $variable);
     }
 
     public function system(string $variable = '')
     {
-
-        if (! isset($this->config['system'])) {
-            throw new Exception("System config doesn't exist");
-        }
-
-        $config = $this->config['system'];
-
-        if ($variable === '') {
-            return $config;
-        }
-
-        if (! array_key_exists($variable, $config)) {
-            throw new Exception("Variable {$variable} doesn't exist in system config");
-        }
-
-        return $config[$variable];
+        return $this->get('system', $variable);
     }
 
     public function shop(string $variable = '')
     {
 
-        if (! isset($this->config['shop'])) {
-            throw new Exception("Shop config doesn't exist");
+        return $this->get('shop', $variable);
+    }
+
+    protected function getConfigFromFiles()
+    {
+        $config_files_dir = array_diff(scandir($this->configDir), array('..', '.'));
+
+        foreach($config_files_dir as $file_name) {
+            if (in_array($file_name, $this->excludes)) {
+                continue;
+            }
+            
+            $file_config = require $this->configDir . '/' . $file_name;
+            
+            if (is_array($file_config)) {
+                [$name, $data] = $file_config;
+                $this->config[$name] = $data;
+            }
+        }
+    }
+
+    protected function get(string $config_name, string $variable = '')
+    {
+        if (! isset($this->config[$config_name])) {
+            throw new Exception("{$config_name} config doesn't exist");
         }
 
-        $config = $this->config['shop'];
+        $config = $this->config[$config_name];
 
         if ($variable === '') {
             return $config;
         }
 
         if (! array_key_exists($variable, $config)) {
-            throw new Exception("Variable {$variable} doesn't exist in shop config");
+            throw new Exception("Variable {$variable} doesn't exist in {$config_name} config");
         }
 
         return $config[$variable];
