4b2577336b6e0be23b31465cd26d4162e73c0d6d
Szymon Lewandowski <szymon.levy123@gmail.com>
2025-12-02 21:05:29 +0100

    add HtmlResponse return to CsrfMiddleware

--- src/Middlewares/CsrfMiddleware.php
+++ src/Middlewares/CsrfMiddleware.php
@@ -5,7 +5,7 @@ namespace Middlewares;
 use Closure;
 use Core\Http\Csrf;
 use Core\Http\Request;
-use Middlewares\Exceptions\CsrfException;
+use Core\Http\Response\HtmlResponse;
 
 class CsrfMiddleware
 {
@@ -18,7 +18,7 @@ class CsrfMiddleware
         $token = $request->post('csrf');
 
         if ($token === '' || ! $this->csrf->checkIfTokenValid($token)) {
-            throw new CsrfException("Invalid CSRF token");
+            return new HtmlResponse('', 403);
         }
 
         return $next($request);
