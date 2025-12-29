cfdc2c44adfcd92ff649dd8d71e31015f488e3ed
Szymon Lewandowski <szymon.levy123@gmail.com>
2025-12-29 21:39:30 +0100

    add UrlSegments to Value Objects

---
+++ src/Core/ValueObjects/UrlSegments.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Core\ValueObjects;
+
+class UrlSegments
+{
+    public function __construct(
+        protected array $segments
+    ){}
+
+    public static function fromUri(string $uri): self
+    {
+        $normalizedUri = trim($uri, '/');
+
+        $segments = ($normalizedUri === '') ? [] : explode('/', $normalizedUri);
+
+        return new self($segments);
+    }
+
+    public function segments(): array
+    {
+        return $this->segments;
+    }
+
+    public function cutLast(): self
+    {
+        $segments = $this->segments;
+
+        array_pop($segments);
+
+        return new self($segments);
+    }
+}
