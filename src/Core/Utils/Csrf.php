fa093c3b51070de9a1034ed89e24e515db2bad90
Szymon Lewandowski <szymon.levy123@gmail.com>
2026-02-14 16:18:24 +0100

    add TokenGenerator dependency to Csrf class

--- src/Core/Utils/Csrf.php
+++ src/Core/Utils/Csrf.php
@@ -3,32 +3,29 @@
 namespace Core\Utils;
 
 use Core\Http\Session;
+use Core\Security\TokenGenerator;
 
 class Csrf
 {
-    protected Session $session;
     protected string $token;
 
-    public function __construct(Session $session)
+    public function __construct(
+        protected Session $session,
+        protected TokenGenerator $tokenGenerator
+    )
     {
-        $this->session = $session;
-
         if ($session->has('csrf_token')) {
             $this->token = $this->session->get('csrf_token');
         } else {
-            $this->token = $this->generateToken();
+            $this->token = $this->tokenGenerator->generate();
+
             $this->session->set('csrf_token', $this->token);
         }
     }
 
-    protected function generateToken()
-    {
-        return bin2hex(random_bytes(32));
-    }
-
     public function setInCookie()
     {
-        if (!isset($_COOKIE['csrf_token'])) {
+        if (! isset($_COOKIE['csrf_token'])) {
             setcookie('csrf_token', $this->token, 0, '/', '', false, false);
         }
     }
