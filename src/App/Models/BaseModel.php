21b2ab4e2ea862d38c0f9e679fa96bd5901cf54f
Szymon Lewandowski <szymon.levy123@gmail.com>
2026-02-14 16:06:24 +0100

    remove helpers dependency from Models

--- src/App/Models/BaseModel.php
+++ src/App/Models/BaseModel.php
@@ -5,13 +5,11 @@ namespace App\Models;
 use Core\Config\Config;
 use Core\Database\DataBase;
 use Core\TemplateEngine\TemplateEngine;
-use Core\Utils\Helpers;
 
 abstract class BaseModel
 {
     public function __construct(
         protected Database $database,
-        protected Helpers $helpers,
         protected TemplateEngine $templateEngine,
         protected Config $config
     ){}
